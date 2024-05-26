4ce5074115f367f9c8592fca2fd85eb61349b82c
jisu-d <lim16111611@gmail.com>
2024-05-26 16:33:27 +0900

    hh

--- my-app/src/routes/select_imgs/ImageGroup.svelte
+++ my-app/src/routes/select_imgs/ImageGroup.svelte
@@ -8,6 +8,8 @@
   $: value = $user_data.capture_imgs;
   
   onMount(() => {
+    console.log($user_data);
+    
     // if ($user_data.capture_imgs.length === 0) {
     //   alert('정상적인 접근이 아닙니다.');
     //   goto('/start_page');
