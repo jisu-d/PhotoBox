4839a44afdeedd8ee198cd440a8c5d407dfac799
jisu-d <lim16111611@gmail.com>
2024-07-04 17:45:58 +0900

    gg

--- front/my-app/src/routes/select_design/frame_imgs.ts
+++ front/my-app/src/routes/select_design/frame_imgs.ts
@@ -19,9 +19,16 @@ export const frameimg: FRAME_IMG = [
     ],
     [
         {
+            title: '3-1',
+            src: '/frame/3/1.png',
+            style: 'top: -5%;  right: -10%; position: relative; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; transition: z-index 0.3s, transform 0.3s ease-in-out; width: 80%; max-width: calc(min(50vh, 50vw));',
+            cropp_size: '4:5',
+            cover: true
+        },
+        {
             title: '2-1',
             src: '/frame/2/1.png',
-            style: 'top: -5%;  right: -10%; position: relative; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; transition: z-index 0.3s, transform 0.3s ease-in-out; width: 80%; max-width: calc(min(50vh, 50vw));',
+            style: 'top: 5%; left: -10%; position: relative; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; transition: z-index 0.3s, transform 0.3s ease-in-out; width: 80%; max-width: calc(min(50vh, 50vw));',
             cropp_size: '4:5',
             cover: true
         },
